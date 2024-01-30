4cff151b65a585aa0bd18d94ba3b96bc9520ae41
lukmanAkhmad <lukmand26@gmail.com>
2024-01-30 22:15:37 +0700

    menghapus beberapa komen

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let pwd = document.querySelector('#password').value;
 let confirmPwd = document.querySelector('#confirm-password').value;
 let error = document.querySelector('.error');
 
+
 if (pwd == "" && confirmPwd == "") {
     error.innerHTML = "*Password do not match";
 } else if(pwd === confirmPwd){
