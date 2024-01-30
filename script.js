0be635bf89d6df38d2b76bc84d906fc69d09d03f
lukmanAkhmad <lukmand26@gmail.com>
2024-01-30 21:55:11 +0700

    menambahkan javascript untuk input password

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("hello")
+let pwd = document.querySelector('#password').value;
+let confirmPwd = document.querySelector('#confirm-password').value;
+let error = document.querySelector('.error');
+
+if (pwd == "" && confirmPwd == "") {
+    error.innerHTML = "*Password do not match";
+} else if(pwd === confirmPwd){
+    error.innerHTML = "";
+};
